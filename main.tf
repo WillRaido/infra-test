3fc9e38d4fb69a4dd5c1797df25e516522240a0c
William Quintero <34279620+WillRaido@users.noreply.github.com>
2024-12-01 18:21:28 -0500

    change app1-2 to change services in terraform

--- main.tf
+++ main.tf
@@ -16,10 +16,18 @@ data "aws_eks_cluster_auth" "cluster" {
   name = data.aws_eks_cluster.cluster.name
 }
 
-resource "kubernetes_manifest" "app1" {
+resource "kubernetes_manifest" "app1_deployment" {
   manifest = yamldecode(file("${path.module}/kubernetes/app1-deployment.yaml"))
 }
 
-resource "kubernetes_manifest" "app2" {
+resource "kubernetes_manifest" "app1_service" {
+  manifest = yamldecode(file("${path.module}/kubernetes/app1-service.yaml"))
+}
+
+resource "kubernetes_manifest" "app2_deployment" {
   manifest = yamldecode(file("${path.module}/kubernetes/app2-deployment.yaml"))
+}
+
+resource "kubernetes_manifest" "app2_service" {
+  manifest = yamldecode(file("${path.module}/kubernetes/app2-service.yaml"))
 }
